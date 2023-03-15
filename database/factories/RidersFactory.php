8af43a8fe43d7bebd6222d310c89c950832d4bbc
okarkyaw <okar.k2002@gmail.com>
2023-03-15 11:31:53 +0630

    make riders factory

--- database/factories/RidersFactory.php
+++ database/factories/RidersFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Rider;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
@@ -24,7 +24,13 @@ class RidersFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'phone_number' => fake()->unique()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => encrypt(Str::random(8)),// password
+            'device_id' => Str::random(6),
+            'remember_token' => Str::random(10),
         ];
     }
 }
