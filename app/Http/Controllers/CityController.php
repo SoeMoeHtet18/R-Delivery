446956cfa1b84d7ca5adc769c79183438e3f5eeb
okarkyaw <okar.k2002@gmail.com>
2023-03-20 16:33:47 +0630

    city crud, model and migration

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\City;
+use App\Repositories\CityRepository;
+use App\Services\CityService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request as FacadesRequest;
+use Illuminate\Support\Facades\Validator;
+use Yajra\DataTables\Facades\DataTables;
+
+class CityController extends Controller
+{
+
+    protected $cityRepository;
+    protected $cityService;
+    public function __construct(CityRepository $cityRepository, CityService $cityService)
+    {
+        $this->cityRepository = $cityRepository;
+        $this->cityService    = $cityService;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = City::select('*')->orderBy('id','DESC');
+            return DataTables::of($data)
+                
+                ->addColumn('action', function($row){
+                    $actionBtn = '<a href="' . route("cities.show", $row->id) . '" class="info btn btn-info btn-sm">View</a>
+                    <a href="' . route("cities.edit", $row->id) . '" class="edit btn btn-light btn-sm">Edit</a>
+                    <form action="'.route("cities.destroy", $row->id) .'" method="post" class="d-inline">
+                        <input type="hidden" name="_token" value="'. csrf_token() .'">
+                        <input type="hidden" name="_method" value="DELETE">
+                        <input type="submit" value="Delete" class="btn btn-sm btn-danger"/>
+                    </form>';
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->addIndexColumn()
+                ->make(true);
+        }
+        return view('admin.city.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.city.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $rules = [ 
+            'name'                  => 'required|string',
+        ];
+            
+        $customErr = [
+            'name.required'                 => 'Name field is required.',
+        ];
+        $validator = Validator::make($request->all(), $rules,$customErr);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        } else {
+            $data = $request->all();
+            $this->cityService->saveCityData($data);
+        }
+
+        return redirect()->route('cities.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $city = $this->cityRepository->getByCityID($id);
+        return view('admin.city.detail',compact('city'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $city = $this->cityRepository->getByCityID($id);
+        return view('admin.city.edit',compact('city'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $rules = [ 
+            'name'                  => 'required|string',
+        ];
+            
+        $customErr = [
+            'name.required'                 => 'Name field is required.',
+        ];
+        $validator = Validator::make($request->all(), $rules,$customErr);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        } else {
+            $data = $request->all();
+            $city = $this->cityRepository->getByCityID($id);
+            $this->cityService->updateCityByID($data,$city);
+        }
+
+        return redirect(route('cities.show', $id));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $this->cityService->deleteCityByID($id);
+        return redirect()->route('cities.index');
+    }
+}
---
+++ resources/views/admin/city/detail.blade.php
@@ -0,0 +1,41 @@
+@extends('admin.layouts.master')
+
+@section('content')
+<style>
+    .card-toolbar{
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+    }
+    .create-button { 
+        width: 70px;
+        height: 30px;
+        margin-bottom: 10px;
+    }
+</style>
+        <div class="card card-container">
+            <div class="card-body">
+                    <h2 class="ps-1">
+                        <strong>City Detail</strong>
+                    </h2>
+                    <div class="card-toolbar">
+                    <div class="create-button">
+                    <a href="{{route('cities.edit' , $city->id)}}" class="btn btn-light">Edit</a>
+                    </div>
+                    <form action="{{route('cities.destroy', $city->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Delete" class="btn btn-danger float-end">
+                    </form>
+                </div>
+                    <div class="row m-0 mb-3">
+                        <div class="col-2">
+                            <h4>Name <b>:</b></h4>
+                        </div>
+                        <div class="col-10 p-3">
+                            {{ $city->name }}
+                        </div>
+                    </div>
+            </div>
+        </div>
+@endsection
