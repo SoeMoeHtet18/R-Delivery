973be684de83256f95a40f162c49dae0c20574ff
okarkyaw <okar.k2002@gmail.com>
2023-03-22 18:24:59 +0630

    RD-88 Customer payment validation

--- app/Http/Controllers/Api/CustomerPaymentApiController.php
+++ app/Http/Controllers/Api/CustomerPaymentApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CustomerPaymentRequest;
 use App\Services\CustomerPaymentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -15,21 +16,12 @@ class CustomerPaymentApiController extends Controller
         $this->customerPaymentService = $customerPaymentService;
     }
 
-    public function insertCustomerPayment(Request $request)
-    {
-        $validator = Validator::make($request->all(),[
-            'amount' => 'required',
-            'order_id' => 'required',
-            'type'   => 'required',
-        ]);
+    public function insertCustomerPayment(CustomerPaymentRequest $request)
+    {  
+        $data = $request->all();
+        $customer_payment = $this->customerPaymentService->saveCustomerPayment($data);
 
-        if ($validator->fails()) {
-            return response()->json(['data' => [], 'message' => $validator->messages()->first(), 'status' => 'fail'], 401);
-        } else {
-            $data = $request->all();
-            $customer_payment = $this->customerPaymentService->saveCustomerPayment($data);
-
-            return response()->json( ['data' => $customer_payment, 'message' => 'Successfully Create Customer Payment ', 'status' => 'success'], 200); 
-        }
+        return response()->json( ['data' => $customer_payment, 'message' => 'Successfully Create Customer Payment ', 'status' => 'success'], 200); 
+        
     }
 }
---
+++ app/Http/Requests/CustomerPaymentRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerPaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'order_id'            => 'required',
+            'amount'              => 'required',
+            'type'                => 'required', 
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'order_id.required'          => 'Order field is required',
+            'amount.required'            => 'Amount field is required',
+            'type.required'              => 'Customer Phone Number field is required',
+        ];
+    }
+}
