315721b3f309689def4d4585ea99300c36d06bd5
htoohtetwin4 <htoohtetwin.hhw4@gmail.com>
2023-03-23 19:51:24 +0630

    RD-101 SoftDelete for Order/ItemType/ShopPayment/CustomerPayment/TransactionsForShop

--- app/Models/CustomerPayment.php
+++ app/Models/CustomerPayment.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CustomerPayment extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
     protected $fillable = [
         'order_id', 'amount', 'type', 'proof_of_payment', 'paid_at', 'last_updated_by'
     ];
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
     protected $fillable = [
         'order_code', 'customer_name', 'customer_phone_number', 'township_id', 'rider_id', 'shop_id', 'quantity', 'total_amount', 'delivery_fees', 'markup_delivery_fees',
         'remark', 'status', 'item_type', 'full_address', 'schedule_date', 'type', 'collection_method', 'proof_of_payment', 'last_updated_by'
