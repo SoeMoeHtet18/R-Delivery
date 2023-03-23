6ae38cc7b5062d1ea9a36ddb7a4b8c52be0a40d2
okarkyaw <okar.k2002@gmail.com>
2023-03-23 19:58:38 +0630

    RD-100 add softdelete in rider, shop, shopusers, user and township

--- app/Models/Rider.php
+++ app/Models/Rider.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 
 class Rider extends Authenticatable
 {
-    use HasFactory, HasApiTokens;
+    use HasFactory, HasApiTokens, SoftDeletes;
     protected $fillable = [
         'name', 'phone_number', 'email', 'email_verified_at', 'password', 'device_id','token','refresh_token'
     ];
--- app/Models/ShopUser.php
+++ app/Models/ShopUser.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 
 class ShopUser extends Authenticatable
 {
-    use HasFactory, HasApiTokens;
+    use HasFactory, HasApiTokens, SoftDeletes;
     protected $guarded = [];
     protected $fillable = [
         'name', 'phone_number', 'email', 'email_verified_at', 'password', 'device_id','token', 'refresh_token'
