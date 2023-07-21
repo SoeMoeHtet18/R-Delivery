5cd8dc7241a349ca957ef050d8351e139c595da8
soemoehtet <soemoehtet18203@gmail.com>
2023-07-21 21:07:57 +0630

    added extra charges and discount in orders table

---
+++ database/migrations/2023_07_21_193756_added_extra_charges_and_discount_columns_in_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->decimal('extra_charges')->nullable();
+            $table->decimal('discount')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('extra_charges');
+            $table->dropColumn('discount');
+        });
+    }
+};
