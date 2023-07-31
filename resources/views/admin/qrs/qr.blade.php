e4e097621863d4f030f6a4fcabb30032db2e3723
ThetHlaing <th.ucsy@gmail.com>
2023-07-31 18:25:45 +0700

    Try With Base 64 logo image

--- resources/views/admin/qrs/qr.blade.php
+++ resources/views/admin/qrs/qr.blade.php
@@ -58,7 +58,7 @@
                 <div><b class="content">Township :</b><span class="font"> {{$data['township']}}</span></div>
             </div>
             <div class="right">
-                <img src="http://127.0.0.1:8000/images/tcp_delivery.jpg" alt="logo">
+                 <img src="data:image/png;base64, {{ base64_encode(config('app.url). 'images/tcp_delivery.jpg' }}">
             </div>
             <div class="bot">
                 <b class="content">Hotline :</b><span class="font"> 09740814035</span>
